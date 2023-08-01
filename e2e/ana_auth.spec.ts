b5cde9ef58d45bf1ff0ce0888a3fd98d17d1aa9f
TEPPEI YAMAGUCHI <teyamagu@hotmail.com>
2023-08-01 23:23:22 +0900

    add save auth.json test case

--- e2e/ana_auth.spec.ts
+++ e2e/ana_auth.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('ana login test', async ({ page }) => {
+  const authFile = '.auth/auth.json';
+
   await page.goto('https://www.ana.co.jp/');
   await page.getByRole('link', { name: 'ログイン' }).click();
   await page.getByPlaceholder('例）1234567890').fill(process.env.ANA_USERNAME);
@@ -8,6 +10,8 @@ test('ana login test', async ({ page }) => {
   await page.getByRole('link', { name: 'ログイン', exact: true }).click();
   await page.getByRole('link', { name: 'ログイン', exact: true }).click();
   await expect.soft(page.getByRole('link', { name: 'ログアウト' })).toBeVisible();
+
+  await page.context().storageState({ path: authFile });
 });
 
 test('ana login view test', async ({ page }) => {
